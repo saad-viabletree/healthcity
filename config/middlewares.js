a3f52bd02d4c5e3a3868f57737008159d3d8073c
saad <saad.alam@viabletree.com>
2024-08-01 06:54:26 -0700

    feat: s3 integrated

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,7 +1,34 @@
 module.exports = [
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'healthcity-strapi-be.s3.us-west-1.amazonaws.com',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'healthcity-strapi-be.s3.us-west-1.amazonaws.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+
+
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
